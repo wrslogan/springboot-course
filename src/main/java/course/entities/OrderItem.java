bd17c28077c6c4e462687d85d145f23cf76d2606
Wallace Silva <wrslogan@outlook.com>
2021-11-29 10:50:29 -0300

    Subtotal & Total methods

--- src/main/java/course/entities/OrderItem.java
+++ src/main/java/course/entities/OrderItem.java
@@ -18,7 +18,7 @@ public class OrderItem implements Serializable {
 
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
 
@@ -63,6 +63,10 @@ public class OrderItem implements Serializable {
 
 	public void setPrice(Double price) {
 		this.price = price;
+	}
+
+	public Double getSubTotal() {
+		return price * quantity;
 	}
 
 	@Override
